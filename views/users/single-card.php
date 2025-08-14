81c12c919a1b0f69f043935117ae6ddbfcd8a48c
qcviet <qcviet2012@gmail.com>
2025-08-14 22:27:58 +0700

    Add single-card.php

---
+++ views/users/single-card.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Users: Single Card Service
+ *
+ * @package shopgauyeu
+ * @author quocviet
+ * @since 0.0.1
+ */
+?>
+
+<?php include __DIR__ . '/../includes/global.php'; ?>
+<?php include __DIR__ . '/header.php'; ?>
+<?php include __DIR__ . '/subnav.php'; ?>
+<div class="single-card py-5">
+    <div class="container">
+        <h1 class="single-card__title text-center mt-2">TẶNG THIỆP MIỄN PHÍ <br>🌟🌟🌟</h1>
+        <p class="single-card__title text-center">DỊCH VỤ VIẾT THIỆP CHÚC MỪNG MIỄN PHÍ - TẠO DẤU ẤN ĐẶC BIỆT CHO MÓN QUÀ CỦA BẠN</p>
+        <div class="single-card__image">
+            <img src="<?php echo BASE_URL . '/assets/images/Asset-4.png'; ?>" alt="Free Card Service" class="img-fluid w-100">
+        </div>
+        <div>
+            <h2 class="single-card__description">Thiệp Chúc Mừng Được Viết Tay<br>
+
+                ▪Mỗi món quà đều kèm theo thiệp chúc mừng được viết tay<br>
+                ▪Nội dung thiệp được tùy chỉnh theo yêu cầu của bạn<br>
+                ▪Thiệp được thiết kế đẹp mắt, phù hợp với từng dịp lễ<br>
+                ▪Hoàn toàn miễn phí khi mua hàng tại Gấu Bông Online<br>
+            </h2>
+            <p>
+                + Bạn có muốn gửi lời chúc đặc biệt đến người thân yêu? Gấu Bông Online sẽ giúp bạn viết những lời chúc ý nghĩa nhất trên thiệp để tạo nên món quà hoàn hảo và đầy cảm xúc!
+            </p>
+        </div>
+        <div class="single-card__grid">
+            <h3 class="text-center">BÀI VIẾT TƯƠNG TỰ</h3>
+
+        </div>
+
+    </div>
+</div>
+
+<?php include __DIR__ . '/category-post.php'; ?>
+
+<?php include __DIR__ . '/footer.php'; ?> 
