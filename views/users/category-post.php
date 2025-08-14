ce561b558f2cb7c9eb6294b2add246200cd86342
qcviet <qcviet2012@gmail.com>
2025-08-14 22:27:48 +0700

    Update category-post.php

--- views/users/category-post.php
+++ views/users/category-post.php
@@ -21,7 +21,7 @@
                 </a>
             </div>
             <div class="col">
-                <a href="<?php echo BASE_URL; ?>?page=single-compression" class="new-products-header">
+                <a href="<?php echo BASE_URL; ?>?page=single-gift-wrapping" class="new-products-header">
                     <div class="new-products-header-content">
                         <img src="<?php echo BASE_URL . '/assets/images/Asset-3.png'; ?>" class="img-fluid"
                             alt="New Products Image">
@@ -30,7 +30,7 @@
                 </a>
             </div>
             <div class="col">
-                <a href="<?php echo BASE_URL; ?>?page=single-ship" class="new-products-header">
+                <a href="<?php echo BASE_URL; ?>?page=single-card" class="new-products-header">
                     <div class="new-products-header-content">
                         <img src="<?php echo BASE_URL . '/assets/images/Asset-4.png'; ?>" class="img-fluid"
                             alt="New Products Image">
@@ -39,7 +39,7 @@
                 </a>
             </div>
             <div class="col">
-                <a href="<?php echo BASE_URL; ?>?page=single-compression" class="new-products-header">
+                <a href="<?php echo BASE_URL; ?>?page=single-washing" class="new-products-header">
                     <div class="new-products-header-content">
                         <img src="<?php echo BASE_URL . '/assets/images/Asset-5.png'; ?>" class="img-fluid"
                             alt="New Products Image">
