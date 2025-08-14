30a5d856dd8deea33cea4971af85ca22e7e1d4e6
qcviet <qcviet2012@gmail.com>
2025-08-14 18:34:50 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,6 +6,6 @@ define('BOOTSTRAP_ICONS', BASE_URL . '/node_modules/bootstrap-icons/font/bootstr
 define('CUSTOM_CSS', BASE_URL . '/assets/css/index.css');
 define('IMG_PATH', BASE_URL . '/assets/images');
 define('JQUERY_JS', BASE_URL . '/assets/js/jquery-3.7.1.min.js');
-define('CUSTOM_JS', BASE_URL . '/assets/js/admin.js');
+define('CUSTOM_JS', BASE_URL . '/assets/js/main.js');
 define('COLLECTION_SLIDER', BASE_URL . '/assets/js/collection-slider.js');
 ?>
