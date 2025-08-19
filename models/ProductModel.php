0259ae8d4c9e699e9d35934c901812a5aa6b2c23
qcviet <qcviet2012@gmail.com>
2025-08-20 03:16:59 +0700

    Update ProductModel.php

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -56,6 +56,7 @@ class ProductModel {
                         p.*, 
                         c.category_name,
                         (SELECT MIN(v.price) FROM product_variants v WHERE v.product_id = p.product_id) AS price,
+                        (SELECT v1.variant_id FROM product_variants v1 WHERE v1.product_id = p.product_id ORDER BY v1.price ASC LIMIT 1) AS min_variant_id,
                         (SELECT COALESCE(SUM(v2.stock), 0) FROM product_variants v2 WHERE v2.product_id = p.product_id) AS stock,
                         (SELECT GROUP_CONCAT(DISTINCT v3.price ORDER BY v3.price ASC SEPARATOR ',') FROM product_variants v3 WHERE v3.product_id = p.product_id) AS price_list,
                         (SELECT pi.image_url FROM product_images pi WHERE pi.product_id = p.product_id ORDER BY pi.image_id ASC LIMIT 1) AS image_url
@@ -114,6 +115,7 @@ class ProductModel {
                         p.*, 
                         c.category_name,
                         MIN(v.price) AS price,
+                        (SELECT v1.variant_id FROM product_variants v1 WHERE v1.product_id = p.product_id ORDER BY v1.price ASC LIMIT 1) AS min_variant_id,
                         COALESCE(SUM(v.stock), 0) AS stock,
                         (SELECT pi.image_url FROM product_images pi WHERE pi.product_id = p.product_id ORDER BY pi.image_id ASC LIMIT 1) AS image_url
                   FROM " . $this->table_name . " p 
